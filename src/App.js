b530d305ad051493e4435e7357356b8822d564ec
entropy9 <brian.pene@gmail.com>
2021-02-08 20:34:25 -0800

    react-airtable-app9

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends React.Component {
 	}
 
 	componentDidMount() {
-		fetch('https://api.airtable.com/v0/app38HX2SzhS41J5M/Table%201?api_key=keyTmoW0bFkGJdz6z')
+		fetch('https://api.airtable.com/v0/app38HX2SzhS41J5M/tblgHoGdUOdB7m96S?sort%5B0%5D%5Bfield%5D=published&sort%5B0%5D%5Bdirection%5D=asc&api_key=keyTmoW0bFkGJdz6z')
 			.then(res => res.json())
 			.then(res => {
 				console.log(res.records)
@@ -47,7 +47,7 @@ class App extends React.Component {
 			})
 			.catch(error => console.log(error))
 	}
-
+	
   render() {
 	
     const {booksData} = this.state
