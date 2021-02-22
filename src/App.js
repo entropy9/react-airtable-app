8fdd4f20f16a16ae700583505da510ca79b2c972
entropy9 <brian.pene@gmail.com>
2021-02-21 18:46:34 -0800

    Added search and filter

--- src/App.js
+++ src/App.js
@@ -27,6 +27,11 @@ const useStyles = makeStyles((theme) => ({
   }));
 
 
+  function handleChange(e) {
+	console.log(e.target.value);
+	global.foo = e.target.value;
+ alert(global.foo);
+}
 
 const openInNewTab = (url) => {
 	const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
@@ -39,8 +44,14 @@ class App extends React.Component {
 		booksData: []
 	}
 
-	componentDidMount() {
-		fetch('https://api.airtable.com/v0/app38HX2SzhS41J5M/tblgHoGdUOdB7m96S?sort%5B0%5D%5Bfield%5D=title&sort%5B0%5D%5Bdirection%5D=asc&api_key=keyTmoW0bFkGJdz6z')
+	componentDidMount(myurl) {
+		if(!myurl){
+		myurl ="https://api.airtable.com/v0/app38HX2SzhS41J5M/Table%201?filterByFormula=SEARCH(%22 %22%2C+title)&sort%5B0%5D%5Bfield%5D=title&sort%5B0%5D%5Bdirection%5D=asc&api_key=keyTmoW0bFkGJdz6z"
+		}
+		else{
+
+		}
+		fetch(myurl)
 			.then(res => res.json())
 			.then(res => {
 				console.log(res.records)
@@ -49,14 +60,23 @@ class App extends React.Component {
 			.catch(error => console.log(error))
 	}
 	
-	handleChange = event => {
-		this.setState({ filter: event.target.value });
+	handleChange = e => {
+		console.log(e.target.value);
+	global.foo1 = e.target.value.toLowerCase();
+	 this.componentDidMount("https://api.airtable.com/v0/app38HX2SzhS41J5M/Table%201?filterByFormula=SEARCH(%22"+global.foo1+"%22%2C+LOWER(title))&sort%5B0%5D%5Bfield%5D=title&sort%5B0%5D%5Bdirection%5D=asc&api_key=keyTmoW0bFkGJdz6z")
+
+		
 	  };
+
+	searchAuthor = e => {
+		console.log(e.target.value);
+	global.foo1 = e.target.value.toLowerCase();
+	 this.componentDidMount("https://api.airtable.com/v0/app38HX2SzhS41J5M/Table%201?filterByFormula=SEARCH(%22"+global.foo1+"%22%2C+LOWER(author))&sort%5B0%5D%5Bfield%5D=title&sort%5B0%5D%5Bdirection%5D=asc&api_key=keyTmoW0bFkGJdz6z")
+
+		
+	  };
+
 	
-	  
-
-
-
   render() {
 
 	
@@ -73,15 +93,20 @@ class App extends React.Component {
         return (
             <Grid container direction='row' spacing={2}>
 				
-			<AppBar position="fixed">
+			<AppBar style = {{ background: '#eee' }} position="fixed">
 			<Toolbar>
 				
 		    <Button href='https://airtable.com/invite/l?inviteId=invbTZrBaH9gs4Fvt&inviteToken=007610430b2ec5acfb4f4676204684766f18877568a8028435e77f0edd90eb07' variant="contained" color="secondary" >Airtable</Button>
-			<Button href='https://autodesk.com'variant="contained">Autodesk</Button>       
+			<Button size="small" style = {{ textTransform: 'none', color: '#eee', background: '#333', margin: '22px' }} onClick={() => {this.componentDidMount('https://api.airtable.com/v0/app38HX2SzhS41J5M/Table%201?sort%5B0%5D%5Bfield%5D=author&sort%5B0%5D%5Bdirection%5D=asc&api_key=keyTmoW0bFkGJdz6z');}} variant="contained">Sort by Author</Button> 
+			<Button size="small" style = {{ textTransform: 'none', color: '#eee', background: '#333' }} onClick={() => {this.componentDidMount('https://api.airtable.com/v0/app38HX2SzhS41J5M/Table%201?sort%5B0%5D%5Bfield%5D=title&sort%5B0%5D%5Bdirection%5D=asc&api_key=keyTmoW0bFkGJdz6z');}} variant="contained">Sort by Title</Button> 
+			<Button size="small" style = {{ textTransform: 'none', color: '#eee', background: '#333', margin: '22px' }} onClick={() => {this.componentDidMount('https://api.airtable.com/v0/app38HX2SzhS41J5M/Table%201?sort%5B0%5D%5Bfield%5D=published&sort%5B0%5D%5Bdirection%5D=asc&api_key=keyTmoW0bFkGJdz6z');}} variant="contained">Sort by Date</Button>       
 
 				<Typography variant="h4">
 				              
 				</Typography>
+
+				<input style = {{ margin: '22px' }}  placeholder="Search Titles..." name="firstName" onChange={this.handleChange} />
+				<input style = {{ margin: '22px' }}  placeholder="Search Authors..." name="firstName" onChange={this.searchAuthor} />
 			</Toolbar>
 			</AppBar>
 
